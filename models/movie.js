ac531cd8a1f0bba67265977b2d4b7aff4e36a140
Ayrat Mukhametshin <airatmm@gmail.com>
2022-06-16 10:17:29 +0300

    movie model

--- models/movie.js
+++ models/movie.js
@@ -1,13 +1,77 @@
 const mongoose = require('mongoose');
+const isURL = require('validator/lib/isEmail');
 
+// const regLink = /(http|https):\/\/(www)?\.?([A-Za-z0-9.-]+)\.([A-z]{2,})((?:\/[+~%/.\w-_]*)?\??(?:[-=&;%@.\w_]*)#?(?:[\w]*))?/gi;
+//   image: {
+//     type: String,
+//     required: true,
+//     validate: {
+//       validator: (link) => regLink.test(link),
+//       message: 'Неправильный формат ссылки',
+//     },
+//   },
 
 const movieSchema = new mongoose.Schema({
   country: {
     type: String,
-    minlength: 2,
-    maxlength: 30,
-    required: false,
-    default: 'Жак-Ив Кусто',
+    required: true,
+  },
+  director: {
+    type: String,
+    required: true,
+  },
+  duration: {
+    type: Number,
+    required: true,
+  },
+  year: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (link) => isURL(link),
+      message: 'Неправильный формат ссылки',
+    },
+  },
+  trailerLink: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (link) => isURL(link),
+      message: 'Неправильный формат ссылки',
+    },
+  },
+  thumbnail: {
+    type: String,
+    required: true,
+    validate: {
+      validator: (link) => isURL(link),
+      message: 'Неправильный формат ссылки',
+    },
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+    required: true,
+  },
+  movieId: {
+    type: Number,
+    required: true,
+  },
+  nameRU: {
+    type: String,
+    required: true,
+  },
+  nameEN: {
+    type: String,
+    required: true,
   },
 });
 
